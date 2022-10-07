aa4dd43d13e0e80f042e4bc85a95f571b2a96d57
Amisha <113248863+subi15github@users.noreply.github.com>
2022-10-07 22:16:04 +0530

    Add files via upload

---
+++ Day4_LAB/Exp_21.py
@@ -0,0 +1,6 @@
+#Write a program to convert Decimal number to Binary number using any high level 
+#language.
+print("Enter a decimal number:")
+number=int(input())
+a=bin(number)
+print("Binary Conversion is:",a)
