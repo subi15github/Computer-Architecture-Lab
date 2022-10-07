6e82138d7c3eea34f861aa0fdb75d709073f2697
Amisha <113248863+subi15github@users.noreply.github.com>
2022-10-07 22:19:10 +0530

    Add files via upload

---
+++ Day4_LAB/Exp_22.py
@@ -0,0 +1,6 @@
+#Write a program to convert Decimal number to an Octal number using any high level 
+#language.
+print("Enter a decimal number:")
+number=int(input())
+a=oct(number)
+print("Binary Conversion is:",a)
